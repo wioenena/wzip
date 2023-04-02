32c31dc90c39af6c455b68256a13b57e39c009c7
wioenena-q <wioenena@gmail.com>
2023-04-02 16:03:44 +0300

    test: simple tested

--- src/main.v
+++ src/main.v
@@ -1,5 +1,35 @@
 module main
 
+import szip
+import wzip
+
 fn main() {
-	println('Hello World!')
+	mut zip := szip.open('output.zip', .best_compression, .write)!
+	defer {
+		zip.close()
+	}
+
+	mut dir_entry := &wzip.WZipDirEntry{
+		path: 'my_directory'
+		files: []
+	}
+
+	mut second_dir_entry := &wzip.WZipDirEntry {
+		path: "my_second_directory",
+		files: [
+			&wzip.WZipFileEntry {
+				path: "world.txt",
+				bytes: "World!".bytes()
+			}
+		]
+	}
+
+	file_entry := &wzip.WZipFileEntry{
+		path: 'hello.txt'
+		bytes: 'Hello'.bytes()
+	}
+
+	dir_entry.add_file(file_entry)
+	dir_entry.add_file(second_dir_entry)
+	dir_entry.create(mut zip)!
 }
